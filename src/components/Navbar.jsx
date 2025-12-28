8c7d0b434b7fae49574f30a7ce6c25c3d37f29ea
Bhaskar Sah <bhaskarsah63@gmail.com>
2025-12-28 14:48:04 +0530

    Modified navbar for large as well as small screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { MenuIcon, XIcon } from 'lucide-react'
 
 const Navbar = () => {
 
@@ -14,27 +15,29 @@ const Navbar = () => {
 
                     {/* Desktop Menu */}
                     <div className='hidden sm:flex items-center gap-4 md:gap-8 max-md:text-sm text-gray-800'>
-                        <a href="#" onClick={() => scrollTo(0, 0)}> Home </a>
-                        <a href="#" onClick={() => scrollTo(0, 0)}> Products </a>
-                        <a href="#" onClick={() => scrollTo(0, 0)}> About </a>
-                        <a href="#" onClick={() => scrollTo(0, 0)}> Contact </a>
+                        <Link to="/" onClick={() => scrollTo(0, 0)}> Home </Link>
+                        <Link to="/marketplace" onClick={() => scrollTo(0, 0)}> Marketplace </Link>
+                        <Link to="/messages" onClick={() => scrollTo(0, 0)}> Messages </Link>
+                        <Link to="/my-listings" onClick={() => scrollTo(0, 0)}> My Listings </Link>
+
                     </div>
 
                     <div>
                         <button className='max-sm:hidden cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
-                        <svg onClick={() => setMenuOpen(true)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="sm:hidden"><path d="M4 5h16" /><path d="M4 12h16" /><path d="M4 19h16" /></svg>
+                        <MenuIcon onClick={() => setMenuOpen(true)} className='sm:hidden' />
                     </div>
 
                 </div>
                 {/* Mobile Menu */}
                 <div className={`sm:hidden fixed inset-0 ${menuOpen ? 'w-full' : 'w-0'} overflow-hidden bg-white backdrop-blur shadow-xl rounded-lg z-[200] text-sm transition-all`}>
                     <div className='flex flex-col items-center justify-center h-full text-xl font-semibold gap-6 p-4'>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> Home </a>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> Products </a>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> About </a>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> Contact </a>
+                        <Link to="/" onClick={() => setMenuOpen(false)}> Home </Link>
+                        <Link to="/marketplace" onClick={() => setMenuOpen(false)}> Marketplace </Link>
+                        <Link to="/messages" onClick={() => setMenuOpen(false)}> Messages </Link>
+                        <Link to="/my-listings" onClick={() => setMenuOpen(false)}> My Listings </Link>
+
                         <button className=' cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
-                        <svg onClick={() => setMenuOpen(false)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="absolute size-8 right-6 top-6 text-gray-500 hover:text-gray-700 cursor-pointer"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
+                        <XIcon onClick={() => setMenuOpen(false)} className='absolute size-8 top-6 right-6 text-gray-500 hover:text-gray-700 cursor-pointer' />
                     </div>
                 </div>
             </nav>
