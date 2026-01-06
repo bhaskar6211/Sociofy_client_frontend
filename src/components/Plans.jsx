9fa281024aa092a6b1a6cf8d6b1fed73a094b0f3
Bhaskar Sah <bhaskarsah63@gmail.com>
2026-01-06 21:35:08 +0530

    remove prem+ sub

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Plans = () => {
   return (
-    <div className='max-w-5xl mx-auto z-20 my-30 max-md:px-4'>
+    <div className='max-w-2xl mx-auto z-20 my-30 max-md:px-4'>
       <div className='text-center'>
         <h2 className='text-gray-700 text-4xl font-semibold'>Choose Your Plan</h2>
         <p className='text-gray-500 text-sm max-w-md mx-auto'>Start at no cost and upgrade as your needs grow. Choose the right plan for your content creation projects.</p>
