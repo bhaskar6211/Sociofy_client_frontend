a43ac36764054c058f993e194c2008b4a4400df3
Bhaskar Sah <bhaskarsah63@gmail.com>
2026-01-06 23:21:29 +0530

    for navigate profilelink of user

--- src/pages/ListingDetails.jsx
+++ src/pages/ListingDetails.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getProfileLink } from '../assets/assets';
 
 const ListingDetails = () => {
+
+  const navigate = useNavigate()
+  const currency = import.meta.env.VITE_CURRENCY || '$';
+  
+  const [listing, setListing] = useState(null);
+  const profileLink = listing && getProfileLink(listing.platform, listing.username)
+
   return (
     <div>
-      <h1>ListingDetails page</h1>
+      
     </div>
   )
 }
