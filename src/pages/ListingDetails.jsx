eb74f2a12e4fee53aadf8dce588a0b8a12177c2f
bhaskar6211 <bhaskarsah63@gmail.com>
2026-01-07 23:16:07 +0530

    Work in manage listing page

--- src/pages/ListingDetails.jsx
+++ src/pages/ListingDetails.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { getProfileLink } from '../assets/assets';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { getProfileLink, platformIcons } from '../assets/assets';
+import { useSelector } from 'react-redux';
+import { ArrowLeftIcon, ArrowUpRightSquareIcon, Loader2Icon } from 'lucide-react';
 
 const ListingDetails = () => {
 
@@ -10,11 +12,51 @@ const ListingDetails = () => {
   const [listing, setListing] = useState(null);
   const profileLink = listing && getProfileLink(listing.platform, listing.username)
 
-  return (
-    <div>
-      
+  const {listingId} = useParams();
+  const {listings} = useSelector((state) => state.listing);
+
+  useEffect(() => {
+    const listing = listings.find((listing) => listing.id === listingId);
+    if (listing) {
+      setListing(listing);
+    }
+  }, [listingId, listings]);
+
+  return listing ? (
+    <div className='mx-auto min-h-screen px-6 md:px-16 lg:px-24 xl:px-32'>
+      <button onClick={() => navigate(-1)} className='flex items-center gap-2 text-slate-600 py-5'>
+        <ArrowLeftIcon className='size-4' /> Go to Previous Page
+      </button>
+
+      <div className='flex items-start max-md:flex-col gap-10'>
+        <div className='flex-1 max-md:w-full'>
+          {/* Top Section */}
+          <div className='bg-white rounded-xl border border-gray-200 p-6 mb-5'>
+            <div className='flex items-start gap-3'>
+              <div className='p-2 rounded-xl'>{platformIcons[listing.platform]}</div>
+              <div>
+                <h2 className='flex items-center gap-2 text-xl font-semibold text-gray-800'>{listing.title}
+                <Link target='_blank' to={profileLink}>
+                  <ArrowUpRightSquareIcon className='size-4 hover:text-indigo-500'/>
+                </Link>
+                </h2>
+                <p>
+                  @{listing.username} - {listing.platform?.charAt(0).toUpperCase() + listing.platform?.slice(1)}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Seller Info and Purchase Options */}
+        <div></div>
+      </div>
+
     </div>
-  )
+  ) : (
+    <div className='h-screen flex justify-center items-center'>
+      <Loader2Icon className='size-7 animate-spin text-indigo-600'/>
+    </div>
+  );
 }
 
 export default ListingDetails
