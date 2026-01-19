df1cff9f060e72f9fd5f3734fc012157b124ad91
bhaskar6211 <bhaskarsah63@gmail.com>
2026-01-19 23:42:29 +0530

    work for the managing the listing page

--- src/pages/MyListings.jsx
+++ src/pages/MyListings.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 
 const MyListings = () => {
+  const {userListings, balance }= useSelector((state)=>state.listing)
+  const currency = import.meta.env.VITE_CURRENCY || '$';
+  const navigate = useNavigate(); 
+
+  const totalValues = userListings.reduce((sum,listing) => sum + (listing.price || 0), 0);
+  const activeListings = userListings.filter((listing) => listing.status === 'active').length;
+  const soldListings = userListings.filter((listing) => listing.status === 'sold').length;
+
   return (
-    <div>
-      <h1>MyListings page</h1>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 pt-8'>
+      
     </div>
   )
 }
