e40b09414573638985f1023637046f5a562ea2bc
bhaskar6211 <bhaskarsah63@gmail.com>
2026-01-20 00:00:49 +0530

    work for header in manage listing page

--- src/pages/MyListings.jsx
+++ src/pages/MyListings.jsx
@@ -1,3 +1,4 @@
+import { Plus } from 'lucide-react';
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
@@ -13,7 +14,21 @@ const MyListings = () => {
 
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 pt-8'>
-      
+      {/* Header */}
+      <div className='flex flex-col md:flex-row justify-between items-start md:items-center mb-8'>
+        <div>
+          <h1 className='text-3xl font-bold text-gray-800'>My Listings</h1>
+          <p className='text-gray-600 mt-1'>Manage your Social media account listings</p>
+        </div>
+        <button onClick={()=>navigate('/create-listing')} className='bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded font-medium flex items-center space-x-2 mt-4 md:mt-0'>
+          <Plus className='size-4' />
+          <span>Add New Listing</span>
+        </button>
+      </div>
+      {/* Stats */}
+      <div className='grid grid-cols-1 md:grid-cols-4 gap-6 mb-8'>
+
+      </div>
     </div>
   )
 }
